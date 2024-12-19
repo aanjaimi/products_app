52baeab1cb76eddc096b0f05a064b8d0d6ac31a5
Ayoub Anjaimi <aanjaimi@student.1337.ma>
2024-12-19 22:59:34 +0100

    chore: change from render to railway

--- frontend/src/lib/fetcher.ts
+++ frontend/src/lib/fetcher.ts
@@ -3,5 +3,5 @@ import axios from 'axios'
 export const fetcher = axios.create({
     withCredentials: true,
 
-    baseURL: process.env.NODE_ENV === 'production' ? 'https://products-app-api.onrender.com' : 'http://localhost:8000',
+    baseURL: process.env.NODE_ENV === 'production' ? 'https://config-production.up.railway.app' : 'http://localhost:8000',
 })

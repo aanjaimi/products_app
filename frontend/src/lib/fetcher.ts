9b77fb5ab1f716156e3249257e9eba4caa8323fc
Ayoub Anjaimi <aanjaimi@student.1337.ma>
2024-12-19 21:39:52 +0100

    chore: update base url to backend url

--- frontend/src/lib/fetcher.ts
+++ frontend/src/lib/fetcher.ts
@@ -3,6 +3,5 @@ import axios from 'axios'
 export const fetcher = axios.create({
     withCredentials: true,
 
-    baseURL: 'http://localhost:8000',
-    // baseURL: process.env.NODE_ENV === 'production' ? 'https://products-app-api.onrender.com' : 'http://localhost:8000',
+    baseURL: process.env.NODE_ENV === 'production' ? 'https://products-app-api.onrender.com' : 'http://localhost:8000',
 })
